b8f7ce6ee6a31fc70f07ff28be385dcd2af78dbb
Данил Чупин <danilcupin@MacBook-Pro-Danil.local>
2023-05-16 10:59:05 +0300

    first commit

---
+++ src/mock/index.ts
@@ -0,0 +1,13 @@
+import { LocalGithubUser } from 'types'
+
+export const defaultUser: LocalGithubUser = {
+	login: 'DanyaChupin',
+	avatar: 'https://avatars.githubusercontent.com/u/105223502?v=4',
+	name: 'Danya Chupin',
+	location: 'Moscow',
+	bio: 'React developer',
+	repos: 10,
+	followers: 0,
+	following: 0,
+	created: '2022-05-09T14:10:02Z',
+}
